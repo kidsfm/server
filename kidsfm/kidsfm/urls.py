0e697cfeca17a9c0dcb8480a4ac3a1a1043c5302
Randy Champagne <r-champagne@neiu.edu>
2017-02-07 15:05:14 -0500

    configured routing and imported templates for blog app

--- kidsfm/kidsfm/urls.py
+++ kidsfm/kidsfm/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls 		import url
+from django.conf.urls 		import include, url
 from django.contrib 		import admin
 from django.views.generic 	import TemplateView
 
@@ -26,6 +26,9 @@ urlpatterns = [
     													content_type="text/html"
     							)
     ),
+
+    # /blog/
+    url(r'^blog/', 				include('blog.urls')),
 
     # /admin/
     url(r'^admin/', 			admin.site.urls),
