3a2a6950f2faeb1f1bc1b774fad89708dd60c576
Randy Champagne <r-champagne@neiu.edu>
2017-02-14 20:28:12 -0500

    config routes, design theme n template for /mission/

--- kidsfm/kidsfm/urls.py
+++ kidsfm/kidsfm/urls.py
@@ -20,19 +20,62 @@ from django.views.generic 	import TemplateView
 
 
 urlpatterns = [
-    # /*
-    url(r'^$', 			TemplateView.as_view(
+
+    ########################
+    # Pages routes
+    ########################
+
+    # /
+    #
+    #    ToDo: 
+    #    - make this route specific to the / url
+    url(r'^$',                  TemplateView.as_view(
     											template_name='kidsfm/index.html', 
     											content_type="text/html"
-    					),
-    					name='index'
+                                ),
+                                name='index'
     ),
 
+    # /*
+    #
+    #    ToDo: 
+    #    - use this route to handle 404 errors
+    #url(r'^$',                  TemplateView.as_view(
+    #                                            template_name='kidsfm/404.html', 
+    #                                            content_type="text/html"
+    #                            ),
+    #                            name='404'
+    #),
+
+    # /mission
+    url(r'^mission/$',          TemplateView.as_view(
+                                                template_name='kidsfm/mission_statements.html', 
+                                                content_type="text/html"
+                                ),
+                                name='mission_statements'
+    ),
+
+    # /mission/{statement_slug}
+    url(r'^mission/(\w+)/$',    TemplateView.as_view(
+                                                template_name='kidsfm/mission_single.html', 
+                                                content_type="text/html"
+                                ),
+                                name='mission_single'
+    ),
+
+
+
+
+
+    ########################
+    # App routes
+    ########################
+    
     # /admin/
     url(r'^admin/', 	admin.site.urls),
 
     # /blog/
-    url(r'^blog/', 		include('blog.urls'),		name='blog'),
+    url(r'^blog/', 	    include('blog.urls'),		name='blog'),
 
     # /images/
     url(r'^images/', 	include('images.urls'),		name='images'),
@@ -41,10 +84,10 @@ urlpatterns = [
     url(r'^oauth2/',    include('oauth2.urls'),     name='oauth2'),
 
     # /schedule/
-    url(r'^schedule/', 	include('schedule.urls'),	name='schedule'),
+    url(r'^schedule/',  include('schedule.urls'),	name='schedule'),
 
     # /team/
-    url(r'^team/', 		include('team.urls'),		name='team'),
+    url(r'^team/',      include('team.urls'),		name='team'),
 
     # /videos/
     url(r'^videos/', 	include('videos.urls'),		name='videos'),
@@ -54,11 +97,11 @@ urlpatterns = [
 
 
     ########################
-    # debug & testing
+    # debug & testing routes
     ########################
     
     # /base
-    url(r'^base/',      TemplateView.as_view(
+    url(r'^base/',     TemplateView.as_view(
                                                 template_name='kidsfm/base.html', 
                                                 content_type="text/html"
                         ),
