32c42a34a7ed16fd46e3a4151b9b6fb99f2131c5
Randy Champagne <r-champagne@neiu.edu>
2017-03-03 11:23:51 -0500

    init mission.Statement model

--- kidsfm/mission/models.py
+++ kidsfm/mission/models.py
@@ -8,9 +8,36 @@ class Statement(models.Model):
 	Statement model
 	defines attributes for a single Statement object of the Mission app
 	'''
+	title			= models.CharField(max_length=50)
+	description		= models.TextField()
+
+
+	# ToDo
+	# - find a way to overwrite images instead of duplicating them
+	# - Note: image should be a square thumbnail
+	# see: http://stackoverflow.com/a/8342249
+	image			= models.ImageField(upload_to='img/mission/statement')
+
+
+	# ToDo:
+	# - create & import cover image choices from theme app
+	# - Note: image should be a rectangle landscape
+	#cover_image	= 
+
+
+	# ToDo:
+	# - create & import icon choices from theme app
+	#icon			= 
+
+
+	# ToDo:
+	# - create & import color choices from theme app
+	#color			= 
+
+
 	def __str__(self):
-		#return '%s %s %s' % (self.first_name, self.middle_name ,self.last_name)
-		pass
+		return '%s' % (self.title,)
+		
 
 
 
