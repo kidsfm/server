4850b472497363b6def075659da6d8bb81dda90b
Randy Champagne <r-champagne@neiu.edu>
2017-03-06 08:54:51 -0500

    added mission.Statement model to admin panel

--- kidsfm/mission/admin.py
+++ kidsfm/mission/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+from django.contrib 	import admin
+from .models 			import Statement
 
-# Register your models here.
+
+# Config admin form for Member model
+class StatementAdmin(admin.ModelAdmin):
+    list_display = [
+    	'title',
+    	'description',
+    ]
+
+# Register Statement model and admin form
+admin.site.register(Statement, StatementAdmin)
+
+
+
+
