5420b5a31117d8cc7b56a2b23e765acd4d68979c
Randy Champagne <r-champagne@neiu.edu>
2017-03-07 15:29:37 -0500

    impl admin form for contact.Location model

--- kidsfm/contact/admin.py
+++ kidsfm/contact/admin.py
@@ -1,8 +1,8 @@
 from django.contrib 	import admin
-from .models 			import Message
+from .models 			import Message, Location
 
 
-# Config admin form for Member model
+# Config admin form for Message model
 class MessageAdmin(admin.ModelAdmin):
     list_display = [
     	'name',
@@ -12,6 +12,20 @@ class MessageAdmin(admin.ModelAdmin):
 
 # Register Message model and admin form
 admin.site.register(Message, MessageAdmin)
+
+
+# Config admin form for Member model
+class LocationAdmin(admin.ModelAdmin):
+    list_display = [
+    	'title',
+    	'address',
+    	'city',
+    	'state',
+    	'country',
+    ]
+
+# Register Location model and admin form
+admin.site.register(Location, LocationAdmin)
 
 
 
