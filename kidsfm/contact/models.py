cb6573ca3edaaf363831e59c5c628196f254d2f2
Randy Champagne <r-champagne@neiu.edu>
2017-03-07 13:21:10 -0500

    contact.Message.email field now limited to 50 chars

--- kidsfm/contact/models.py
+++ kidsfm/contact/models.py
@@ -8,7 +8,7 @@ class Message(models.Model):
 	defines attributes for a single Message object of the Contact app
 	'''
 	name		= models.CharField(max_length=100)
-	email		= models.EmailField()
+	email		= models.EmailField(max_length=50)
 	message		= models.TextField(max_length=255)
 	sent_date	= models.DateTimeField()
 
