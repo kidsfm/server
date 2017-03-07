788001ebee8ad4009aec027232e11abe087a05e4
Randy Champagne <r-champagne@neiu.edu>
2017-03-07 10:45:49 -0500

    contact.Message.message field now limited to 255 chars

--- kidsfm/contact/models.py
+++ kidsfm/contact/models.py
@@ -9,7 +9,7 @@ class Message(models.Model):
 	'''
 	name		= models.CharField(max_length=100)
 	email		= models.EmailField()
-	message		= models.TextField()
+	message		= models.TextField(max_length=255)
 	sent_date	= models.DateTimeField()
 
 
