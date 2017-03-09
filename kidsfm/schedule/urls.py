3739f2319584476a1a11d220a48f95a8e0051208
Randy Champagne <r-champagne@neiu.edu>
2017-03-09 09:27:48 -0500

    impl Index n Programs_json views for schedule app

--- kidsfm/schedule/urls.py
+++ kidsfm/schedule/urls.py
@@ -1,12 +1,31 @@
 from django.conf.urls	import url
-from . 					import views
+from .views 			import Index, Programs, Programs_json
 
 
 urlpatterns = [
 
 	# /schedule/
-	url(r'^$', 								views.Index, 	name='index'),
+	url(
+			r'^$',
+			Index.as_view(),
+			name='index'
+	),
+
 
 	# /schedule/<program-slug>
-	url(r'^(?P<program_slug>[\w\-]+)/$', 	views.Program, 	name='program'),
+	url(
+			r'^programs/(?P<program_slug>[\w\-]+)/$',
+			Programs,
+			name='programs'
+	),
+
+
+	# /schedule/programs/
+	# /schedule/programs?<offset=0&limit=4>
+	url(
+			r'^programs/$',
+			Programs_json.as_view(),
+			name='programs_json'
+	),
+
 ]
--- kidsfm/schedule/views.py
+++ kidsfm/schedule/views.py
@@ -1,23 +1,135 @@
-from django.http		import HttpResponse
-from django.template	import loader
+from django.http			import HttpResponse
+from django.template		import loader
+from django.views.generic	import View
+from django.core 			import serializers
+from django.shortcuts		import render
+from datetime				import datetime
+from .models				import Program
 
 
-def Index(request):
-	"""
-	Program index recheable from /schedule/ URL
-	"""
-	template 	= loader.get_template('schedule/index.html')
-	context 	= {}
-	return HttpResponse(template.render(context,request))
+
+class Index(View):
+	'''
+	Returns an HTML page with an index of Program objects
+
+	URL:	/schedule/
+	'''
+	def get(self, request):
+
+		# define theme settings/properties
+		template_uri = 'schedule/index.html'
 
 
-def Program(request, program_slug):
+		# fetch all data from Program model
+		program_data = fetch_program_data({})
+
+
+		# load data in context container
+		context = {
+			"programs"	: program_data,
+		}
+
+
+		# render template with data & send HTML to client
+		return render(request, template_uri, context)
+
+
+
+def Programs(request, program_slug):
 	'''
 	Single program recheable from /schedule/<program-slug> URL
 	'''
 	template 	= loader.get_template('schedule/program.html')
 	context 	= {}
 	return HttpResponse(template.render(context,request))
+
+
+
+
+class Programs_json(View):
+	'''
+	Returns serialized JSON data enabling client to filter schedule.Program objects via URL-encoded queries.
+
+	URL: 	
+	- /schedule/programs/
+	- /schedule/programs?<offset=0&limit=4>
+
+	ToDo:
+	- validate query & send "bad format" status code if invalid
+	'''
+	def get(self, request):
+
+		# fetch query params
+		query = {
+			"offset" : request.GET.get('offset', None),
+			"limit"	 : request.GET.get('limit', None)
+		}
+
+
+
+		# fetch data
+		program_data = fetch_program_data(query)
+
+
+		# serialize & return data
+		data = serializers.serialize(
+										'json', 
+										list(program_data), 
+										fields=(
+												'title',
+												'description',
+												'start_time',
+												'end_time',
+												'start_date',
+												'end_date',
+												'frequency',
+												'team',
+												)
+									)
+		return HttpResponse(data, content_type="application/json")
+
+
+
+def fetch_program_data(query):
+	'''
+	Helper function that queries the DB for Program objects using filters defined in query.
+	'''
+
+	# fetch role
+	kwargs = dict()
+	try:
+		kwargs['role'] = int(query['role'])
+	except:
+		pass
+	
+
+	# fetch program data from DB
+	programs = Program.objects.filter( **kwargs )
+
+
+	# apply offset
+	try:
+		offset = int(query['offset'])
+		programs = programs[offset:]
+	except:
+		pass
+		
+
+	# apply limit
+	try:
+		limit = int(query['limit'])
+		programs = programs[:limit]
+	except:
+		pass
+
+
+	# return data
+	return programs
+	
+
+
+
+
 
 
 
