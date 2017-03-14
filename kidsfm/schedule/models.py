3621ce6265094dc5eec51a72e69a648aa20cc50e
Randy Champagne <r-champagne@neiu.edu>
2017-03-14 11:30:12 -0400

    impl schedule.Slot model

--- kidsfm/schedule/models.py
+++ kidsfm/schedule/models.py
@@ -17,12 +17,8 @@ class Program(models.Model):
 	# see: http://stackoverflow.com/a/8342249
 	image		= models.ImageField(upload_to='img/schedule/program')
 
-	start_time 	= models.TimeField()
-	end_time	= models.TimeField()
-	start_date	= models.DateField()
-	end_date	= models.DateField()
-	frequency	= models.ManyToManyField('Day')
 	team		= models.ManyToManyField(Member)
+	#time_slots	= models.ManyToManyField('Slot')
 	slug 		= AutoSlugField(
 						max_length=50, 
 						unique=True, 
@@ -43,9 +39,6 @@ class Program(models.Model):
 
 	# ToDo:
 	#
-	# - verify that end_time is scheduled after start_time
-	# - verify that end_date is scheduled after start_date
-	#
 	# - sanetize slug using: from django.utils.text import slugify
 	#	see: https://keyerror.com/blog/automatically-generating-unique-slugs-in-django
 	#	see: https://docs.djangoproject.com/en/1.10/ref/utils/#module-django.utils.text
@@ -64,6 +57,43 @@ class Program(models.Model):
 
 	def __str__(self):
 		return '%s: %s...' % (self.title, self.description[:10])
+
+
+
+
+class Slot(models.Model):
+	'''
+	docstring for Slot
+	'''
+
+	day			= models.ForeignKey('Day', on_delete=models.CASCADE, default=1)
+	program 	= models.ForeignKey('Program', on_delete=models.CASCADE)
+	start_time 	= models.TimeField()
+	end_time	= models.TimeField()
+	start_date	= models.DateField()
+	end_date	= models.DateField()
+
+
+	# ToDo:
+	#
+	# - verify that end_time is scheduled after start_time
+	# - verify that end_date is scheduled after start_date
+	#
+	#def save(self, *args, **kwargs):
+	#	if self.is_primary:
+	#		try:
+	#			temp = Location.objects.get(is_primary=True)
+	#			if self != temp:
+	#				temp.is_primary = False
+	#				temp.save()
+	#		except Location.DoesNotExist:
+	#			pass
+	#	super(Location, self).save(*args, **kwargs)
+
+
+	def __str__(self):
+		return '%s: %s - %s' % (self.day, self.start_time, self.end_time)
+		
 
 
 
