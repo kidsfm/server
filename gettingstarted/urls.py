27e5fc3ea3288e17a4290e388b415f828336c3b0
Randy Champagne <r-champagne@neiu.edu>
2017-07-14 00:52:42 -0400

    successfully rendered base oute in dev

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,16 +1,73 @@
-from django.conf.urls import include, url
+from django.conf.urls 		import include, url
+from django.contrib 		import admin
+#admin.autodiscover()
+from django.views.generic 	import TemplateView
 
-from django.contrib import admin
-admin.autodiscover()
 
-import hello.views
 
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
+
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    url(r'^admin/', include(admin.site.urls)),
+    # /
+    #
+    #    ToDo: 
+    #    - make this route specific to the / url
+    url(
+            r'^$',                  
+            TemplateView.as_view(
+                template_name='kidsfm/index.html', 
+                content_type="text/html"
+            ),
+            name='index'
+    ),
+
+
+
+
+
+    ########################
+    # debug & testing routes
+    ########################
+    
+    # /base
+    url(
+    		r'^base/',     
+            TemplateView.as_view(
+                template_name='kidsfm/base.html', 
+                content_type="text/html"
+            ),
+            name='base'
+    ),
+
 ]
+
+
+
+
+
+
+# This enables built-in dev server to serve files from MEDIA_ROOT & STATIC_ROOT
+# see: http://stackoverflow.com/a/38446584
+from django.conf import settings
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
